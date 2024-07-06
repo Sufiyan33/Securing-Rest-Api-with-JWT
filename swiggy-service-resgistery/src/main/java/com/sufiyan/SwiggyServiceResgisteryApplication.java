8c30091ceeeced9c82acda75af79c50bc1dd0ed8
Sufiyan33 <basu700736@gmail.com>
2024-07-06 21:06:20 +0530

    Created service registry

---
+++ swiggy-service-resgistery/src/main/java/com/sufiyan/SwiggyServiceResgisteryApplication.java
@@ -0,0 +1,13 @@
+package com.sufiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyServiceResgisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyServiceResgisteryApplication.class, args);
+	}
+
+}
