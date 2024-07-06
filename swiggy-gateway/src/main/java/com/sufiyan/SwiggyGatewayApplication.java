91645e6a556ada48a4c8451210044ae59c070338
Sufiyan33 <basu700736@gmail.com>
2024-07-06 21:04:47 +0530

    Created api gatewaye

---
+++ swiggy-gateway/src/main/java/com/sufiyan/SwiggyGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sufiyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiggyGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiggyGatewayApplication.class, args);
+	}
+
+}
