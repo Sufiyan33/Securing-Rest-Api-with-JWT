2fbe3569f8c970f3580ba99a1e253a22126a9d84
Sufiyan33 <basu700736@gmail.com>
2024-07-06 21:29:58 +0530

    Adding annotations and properties

--- swiggy-app/src/main/java/com/sufiyan/SwiggyAppApplication.java
+++ swiggy-app/src/main/java/com/sufiyan/SwiggyAppApplication.java
@@ -2,8 +2,10 @@ package com.sufiyan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SwiggyAppApplication {
 
 	public static void main(String[] args) {
